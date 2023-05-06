cf904b33eaed3676eef848e0ff4222cf01d00b56
krishnasujata <131953034+krishnasujata@users.noreply.github.com>
2023-05-06 10:05:08 +0545

    Server

---
+++ Constaints.dart
@@ -0,0 +1,38 @@
+const Wallpaper = [
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+  {
+    "wallpaper": "https://wallpapers.com/images/featured/2ygv7ssy2k0lxlzu.jpg",
+  },
+];
